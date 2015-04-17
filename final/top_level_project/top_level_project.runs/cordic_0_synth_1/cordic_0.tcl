ac1fccddaba849d983ad7ce4f82e27ed051a67ed
Chris Daffron <cdaffron@vols.utk.edu>
2015-04-17 16:59:33 -0400

    Changed debug probes

--- final/top_level_project/top_level_project.runs/cordic_0_synth_1/cordic_0.tcl
+++ final/top_level_project/top_level_project.runs/cordic_0_synth_1/cordic_0.tcl
@@ -3,26 +3,24 @@
 # 
 
   set_param gui.test TreeTableDev
-  set_param xicom.use_bs_reader 1
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
-set_msg_config -id {Labtools 27-147} -limit 4294967295
 create_project -in_memory -part xc7a100tcsg324-1
 set_property target_language VHDL [current_project]
 set_param project.compositeFile.enableAutoGeneration 0
 set_property default_lib xil_defaultlib [current_project]
 
-read_ip {{C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.xci}}
-set_property used_in_implementation false [get_files {{c:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.dcp}}]
-set_property used_in_implementation false [get_files -all {{c:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.dcp}}]
+read_ip /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.xci
+set_property used_in_implementation false [get_files /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.dcp]
+set_property used_in_implementation false [get_files -all /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.dcp]
 set_msg_config -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property is_locked true [get_files {{C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.xci}}]
+set_property is_locked true [get_files /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.xci]
 
 read_xdc dont_touch.xdc
 set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.cache/wt} [current_project]
-set_property parent.project_dir {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project} [current_project]
+set_property webtalk.parent_dir /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.cache/wt [current_project]
+set_property parent.project_dir /data1/cdaffron/git/ece551hw/final/top_level_project [current_project]
 catch { write_hwdef -file cordic_0.hwdef }
 synth_design -top cordic_0 -part xc7a100tcsg324-1 -mode out_of_context
 
@@ -30,11 +28,11 @@ rename_ref -prefix_all cordic_0_
 write_checkpoint -noxdef cordic_0.dcp
 report_utilization -file cordic_0_utilization_synth.rpt -pb cordic_0_utilization_synth.pb
 if { [catch {
-  file copy -force {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.runs/cordic_0_synth_1/cordic_0.dcp} {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.dcp}
-  write_verilog -force -mode synth_stub {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_stub.v}
-  write_vhdl -force -mode synth_stub {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_stub.vhdl}
-  write_verilog -force -mode funcsim {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_funcsim.v}
-  write_vhdl -force -mode funcsim {C:/Users/cdaffron/Documents/ECE 551/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_funcsim.vhdl}
+  file copy -force /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.runs/cordic_0_synth_1/cordic_0.dcp /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0.dcp
+  write_verilog -force -mode synth_stub /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_stub.v
+  write_vhdl -force -mode synth_stub /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_stub.vhdl
+  write_verilog -force -mode funcsim /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_funcsim.v
+  write_vhdl -force -mode funcsim /data1/cdaffron/git/ece551hw/final/top_level_project/top_level_project.srcs/sources_1/ip/cordic_0/cordic_0_funcsim.vhdl
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "Unable to successfully create or copy supporting IP files."
   return -code error
