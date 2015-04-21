3a52a823f9d41af9f51c5c35c27099ee31a2f204
Chris Daffron <cdaffron@vols.utk.edu>
2015-04-21 19:43:40 -0400

    MATLAB script to generate values

---
+++ final/logConvert.m
@@ -0,0 +1,24 @@
+% Linear data
+% xAxis = 1:18;
+linData = 1:1:131071;
+xAxis = 1:length(linData);
+
+fig1 = figure();
+plot(xAxis, linData);
+% p1 = semilogy(xAxis, linData);
+
+k = 255/((131071).^(1/3));
+% k = 255 / (log10(131071));
+
+logData = k * ((linData).^(1/3));
+% logData = k * log10(linData);
+
+figure();
+plot(xAxis, logData);
+
+linDataT = linData';
+logDataT = logData';
+
+togData = zeros(131071,2);
+togData(:,1) = linDataT(:);
+togData(:,2) = logDataT(:);
