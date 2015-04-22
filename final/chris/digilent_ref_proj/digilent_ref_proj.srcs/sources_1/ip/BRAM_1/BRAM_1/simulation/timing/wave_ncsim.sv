19991e99b73a09404cd1ffcc5b3cee53e996426f
Chris Daffron <cdaffron@vols.utk.edu>
2015-04-21 21:36:17 -0400

    Forced all files to be included with reference project

---
+++ final/chris/digilent_ref_proj/digilent_ref_proj.srcs/sources_1/ip/BRAM_1/BRAM_1/simulation/timing/wave_ncsim.sv
@@ -0,0 +1,18 @@
+
+ 
+ 
+ 
+
+ 
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+
+      waveform add -signals /BRAM_1_tb/status
+      waveform add -signals /BRAM_1_tb/BRAM_1_synth_inst/bmg_port/CLKA
+      waveform add -signals /BRAM_1_tb/BRAM_1_synth_inst/bmg_port/ADDRA
+      waveform add -signals /BRAM_1_tb/BRAM_1_synth_inst/bmg_port/DOUTA
+console submit -using simulator -wait no "run"
